6182a1046e0c99901079a76dfab1fdc8133df8f0
muthukumar <muthupalani1986@gmail.com>
2020-09-03 10:16:21 +0530

    purchase history related changes

--- api/purchase_history.js
+++ api/purchase_history.js
@@ -8,57 +8,6 @@ const passport = require('passport');
 var _ = require('lodash');
 const moment = require('moment');
 import Util from "../common/util";
-
-router.post("/new", passport.authenticate('jwt', { session: false }), (req, res, next) => {
-  const userDetails = Util.getUserDetails(req);
-  const newUser = new User(userDetails.customer_id, userDetails.customer_name, userDetails.pin, userDetails.created_at, userDetails.updated_at);
-  db.query(newUser.addUserSQL(), (err, data) => {
-    if (!err) {
-      res.status(200).json({
-        message: "User added successfully."
-      });
-    } else {
-      res.status(400).json({
-        message: "Bad request"
-      });
-    }
-
-  });
-
-});
-
-router.post("/delete", passport.authenticate('jwt', { session: false }), (req, res, next) => {
-  const customer_id = _.get(req, 'body.customer_id');
-  db.query(User.deleteUserByCustomerIdSQL(customer_id), (err, data) => {
-    if (!err) {
-      res.status(200).json({
-        message: "User deleted successfully."
-      });
-    } else {
-      res.status(400).json({
-        message: "Bad request"
-      });
-    }
-  });
-});
-
-router.post("/update", passport.authenticate('jwt', { session: false }), (req, res, next) => {
-  const userDetails = Util.getUserDetails(req);
-  const user = new User(userDetails.customer_id, userDetails.customer_name, userDetails.pin, userDetails.created_at, userDetails.updated_at);
-  const customer_id = _.get(req, 'body.customer_id');
-  db.query(user.updateUserByCustomerIdSQL(customer_id), (err, data) => {
-    if (!err) {
-      res.status(200).json({
-        message: "User updated successfully."
-      });
-    } else {
-      res.status(400).json({
-        message: "Bad request"
-      });
-    }
-  });
-});
-
 router.post("/:customer_id", passport.authenticate('jwt', { session: false }), (req, res, next) => {
   let customer_id = _.get(req, 'params.customer_id');
   db.query(PurchaseHistory.getPurchaseHistoryByCustomerId(customer_id), (err, data) => {
@@ -67,10 +16,14 @@ router.post("/:customer_id", passport.authenticate('jwt', { session: false }), (
       if (data) {
         response = data;
       }
-      res.status(200).json(response);
+      res.status(200).json({
+        statusCode:'0000',
+        history:response
+      });
     } else {
       res.status(400).json({
-        message: "Bad request"
+        statusCode:'4004',
+        msg: "Bad request"
       });
     }
   });
@@ -85,10 +38,14 @@ router.post("/:customer_id/:purchase_date", passport.authenticate('jwt', { sessi
       if (data) {
         response = data;
       }
-      res.status(200).json(response);
+      res.status(200).json({
+        statusCode:'0000',
+        historyDetails:response
+      });
     } else {
       res.status(400).json({
-        message: "Bad request"
+        statusCode:'4004',
+        msg: "Bad request"
       });
     }
   });
--- api/users.js
+++ api/users.js
@@ -12,31 +12,33 @@ router.post("/login", (req, res, next) => {
   const customer_id = _.get(req, 'body.customer_id');
   const pin = _.get(req, 'body.pin');
   const sql = User.getUserByCustomerIdPinSQL(customer_id, pin);
-  console.log('sql',sql);
   db.query(sql, (err, data) => {
     const userDetails = _.find(data, { 'customer_id': customer_id });
     if (userDetails) {
-      let payload = { customer_id: userDetails.customer_id, customer_name: userDetails.customer_name};
+      let payload = { customer_id: userDetails.customer_id, customer_name: userDetails.customer_name };
       let token = jwt.sign(payload, config.jwtOptions.secretOrKey);
-      res.status(200).json({ statusCode:'0000',msg: 'User successly authenticated', token: token,customer_name: userDetails.email_id, customer_name: userDetails.customer_name});
+      res.status(200).json({ statusCode: '0000', msg: 'User successly authenticated', token: token, customer_name: userDetails.customer_name, customer_id: userDetails.customer_id });
     } else {
       res.status(200).json({
+        statusCode:'4004',
         msg: "Invalid customer id & pin"
       });
     }
   });
 });
 router.post("/new", passport.authenticate('jwt', { session: false }), (req, res, next) => {
-  const userDetails=Util.getUserDetails(req);
-  const newUser = new User(userDetails.customer_id, userDetails.customer_name, userDetails.pin,userDetails.created_at, userDetails.updated_at);
+  const userDetails = Util.getUserDetails(req);
+  const newUser = new User(userDetails.customer_id, userDetails.customer_name, userDetails.pin, userDetails.created_at, userDetails.updated_at);
   db.query(newUser.addUserSQL(), (err, data) => {
     if (!err) {
       res.status(200).json({
-        message: "User added successfully."
+        statusCode:'0000',
+        msg: "User added successfully."
       });
     } else {
       res.status(400).json({
-        message: "Bad request"
+        statusCode:'4004',
+        msg: "Bad request"
       });
     }
 
@@ -45,45 +47,53 @@ router.post("/new", passport.authenticate('jwt', { session: false }), (req, res,
 });
 
 router.post("/delete", passport.authenticate('jwt', { session: false }), (req, res, next) => {
-  const customer_id = _.get(req, 'body.customer_id');  
-  db.query(User.deleteUserByCustomerIdSQL(customer_id), (err, data) => {    
+  const customer_id = _.get(req, 'body.customer_id');
+  db.query(User.deleteUserByCustomerIdSQL(customer_id), (err, data) => {
     if (!err) {
       res.status(200).json({
-        message: "User deleted successfully."
+        statusCode:'0000',
+        msg: "User deleted successfully."
       });
     } else {
       res.status(400).json({
-        message: "Bad request"
+        statusCode:'4004',
+        msg: "Bad request"
       });
     }
   });
 });
 
 router.post("/update", passport.authenticate('jwt', { session: false }), (req, res, next) => {
-  const userDetails=Util.getUserDetails(req);
-  const user = new User(userDetails.customer_id, userDetails.customer_name, userDetails.pin,userDetails.created_at, userDetails.updated_at);
-  const customer_id = _.get(req, 'body.customer_id');    
-  db.query(user.updateUserByCustomerIdSQL(customer_id), (err, data) => {    
+  const userDetails = Util.getUserDetails(req);
+  const user = new User(userDetails.customer_id, userDetails.customer_name, userDetails.pin, userDetails.created_at, userDetails.updated_at);
+  const customer_id = _.get(req, 'body.customer_id');
+  db.query(user.updateUserByCustomerIdSQL(customer_id), (err, data) => {
     if (!err) {
       res.status(200).json({
-        message: "User updated successfully."
+        statusCode:'0000',
+        msg: "User updated successfully."
       });
     } else {
       res.status(400).json({
-        message: "Bad request"
+        statusCode:'4004',
+        msg: "Bad request"
       });
     }
   });
 });
 
 router.post("/:customer_id", passport.authenticate('jwt', { session: false }), (req, res, next) => {
-  let customer_id = _.get(req, 'params.customer_id');   
-  db.query(User.getUserByCustomerId(customer_id), (err, data) => {    
+  let customer_id = _.get(req, 'params.customer_id');
+  db.query(User.getUserByCustomerId(customer_id), (err, data) => {
     if (!err) {
-      res.status(200).json(data[0]);
+      res.status(200).json({
+        statusCode:'0000',
+        userDetails:data[0]
+      });
     } else {
       res.status(400).json({
-        message: "Bad request"
+        statusCode:'0000',
+        msg: "Bad request"
       });
     }
   });
